50071bd414f40475013b118ee20719970fe1542d
Tohidul0 <tohidulalam967418@gmail.com>
2023-11-28 22:14:55 +0600

    28/11/23 solved

---
+++ LeetCode/Roman to Integer.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        map <char , int> roman;
+        roman.insert(make_pair('I' , 1));
+        roman.insert(make_pair('V' , 5));
+        roman.insert(make_pair('X' , 10));
+        roman.insert(make_pair('L' , 50));
+        roman.insert(make_pair('C' , 100));
+        roman.insert(make_pair('D' , 500));
+        roman.insert(make_pair('M' , 1000));
+        int sum=0;
+        int num;
+
+        for(int i=0; i<s.size(); ){
+            if (i== s.size()-1 || roman[s[i]]>= roman[s[i+1]]){
+                num = roman[s[i]];
+                i++;
+
+            }
+            else{
+                num = roman[s[i+1]] - roman[s[i]];
+                i = i+2;
+            }
+            sum = sum + num;
+        }
+        return sum;
+    }
+};
